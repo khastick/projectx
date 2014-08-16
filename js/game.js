af64707752c542316ffccfef6b474120e0d631aa
Richard Huynh <richardbaohuynh@gmail.com>
2014-08-16 13:40:04 -0400

    Starts adding player movement (not yet functional)

--- js/game.js
+++ js/game.js
@@ -16,11 +16,50 @@ function create() {
 	player = game.add.sprite(64, game.world.height - 50, 'enemy');
 	player = game.add.sprite(100, game.world.height - 100, 'boss');
 
+	cursors = game.input.keyboard.createCursorKeys();
+
 	game.world.setBounds(0, 0, 800, 600);
 	player.body.collideWorldBounds(true);
 	player.bringToTop();
+
+
+	//game.physics.arcade.enable(player);
 }
 
 function update() {
-	
+    //  Reset the players velocity (movement)
+    //player.body.velocity.x = 0;
+
+	if (cursors.left.isDown) {
+        //  Move to the left
+        player.body.velocity.x = -150;
+
+       // player.animations.play('left');
+    }
+    else if (cursors.right.isDown) {
+        //  Move to the right
+        player.body.velocity.x = 150;
+
+        //player.animations.play('right');
+    }
+    else if (cursors.up.isDown) {
+    	player.body.velocity.y = -150;
+    }
+    else if (cursors.down.isDown) {
+    	player.body.velocity.y = 150;
+    }
+    else {
+        //  Stand still
+        player.animations.stop();
+
+        //player.frame = 4;
+    }
+    
+    //  Allow the player to jump if they are touching the ground.
+    /*
+    if (cursors.up.isDown && player.body.touching.down)
+    {
+        player.body.velocity.y = -350;
+    }
+    */
 }
